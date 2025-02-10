36c05a4cd9fa43274526961e6805dcb482c5d155
swaroop <swaroopms658@gmail.com>
2025-02-10 19:47:27 +0530

    cors

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # ✅ Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
